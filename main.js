29cf300cc1a3e168ff1419597b76f187bdbd187e
ryansreenivasam <ryansreenivasam@gmail.com>
2020-01-16 09:07:47 -0800

    before adding texture

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ var scene;
 var renderer;
 var world;
 var starArray = [];
+var intro = false;
 
 function init() {
 	// set up the scene
-	createScene();
+    createScene();
+    
+    intro = true;
 
 	//call game loop
 	update();
@@ -39,31 +42,36 @@ function addWorld() {
 }
 
 function addStars() {
-    for(i=0; i<100; i++) {
+    for(i=0; i<150; i++) {
         var geoStar = new THREE.CubeGeometry( 0.2, 0.2, 5 );
         var matStar = new THREE.MeshBasicMaterial({ color: 0xbfdfff });
         var star = new THREE.Mesh( geoStar, matStar );
         scene.add(star);
         starArray[i] = star;
-        star.position.x = (Math.random() - 0.5) * 100;
-        star.position.y = (Math.random() - 0.5) * 100;
+        star.position.x = (Math.random() - 0.5) * window.innerWidth/12;
+        star.position.y = (Math.random() - 0.5) * window.innerHeight/12;
         star.position.z = Math.random() * -100;
     }
 }
 
 function updateStars() {
-    for(i=0; i<100; i++) {
-        if(starArray[i].position.z >= 0) {
-            resetStar(i);
+    if(intro) {
+
+    }
+    else {
+        for(i=0; i<150; i++) {
+            if(starArray[i].position.z >= 0) {
+                resetStar(i);
+            }
+            starArray[i].position.z += 3;
         }
-        starArray[i].position.z += 2;
     }
 }
 
 function resetStar(index) {
     starArray[index].position.z = -100;
-    starArray[index].position.x = (Math.random() - 0.5) * 100;
-    starArray[index].position.y = (Math.random() - 0.5) * 60;
+    starArray[index].position.x = (Math.random() - 0.5) * window.innerWidth/12;
+    starArray[index].position.y = (Math.random() - 0.5) * window.innerHeight/12;
 }
 
 function addLight() {
